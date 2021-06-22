d887adb5978b0b67d9d7168860c936be2d743878
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-06-22 13:56:30 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/arsw/InteractiveBoard.java
@@ -0,0 +1,15 @@
+package edu.escuelaing.arsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class InteractiveBoard {
+    public static void main( String[] args ){
+        SpringApplication.run(InteractiveBoard.class, args);
+    }
+}
